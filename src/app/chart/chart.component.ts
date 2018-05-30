844ec51131d6bcd97519dd6af52379a4d8af5b03
Unknown <zaoralj@gmail.com>
2018-05-30 21:18:14 +0200

    date y

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UUID } from 'angular2-uuid';
+import { DateTime } from 'date-time-js';
 
 declare var Plotly: any;
 
@@ -20,21 +21,65 @@ export class ChartComponent implements OnInit {
   public showChart() {
 
     const xData = [];
-    const yData = [];
+    const yData1 = [];
+    const yData2 = [];
+    const yData3 = [];
 
-    for (let index = 0; index < 360 * 8; index++) {
-      xData.push(index);
-      yData.push(Math.random() * 2 + 10);
+
+
+    for (let index = 0; index < 300; index++) {
+      xData.push(new DateTime().add(index * 3, 'second').format('yyyy-MM-dd HH:mm:ss') );
+      yData1.push(Math.random() * 2 + 5);
+      yData2.push(Math.random() * 2 + 10);
+      yData3.push(Math.random() * 2 + 15);
     }
 
     const trace1 = {
       x: xData,
-      y: yData,
-      type: 'scatter'
+      y: yData1,
+      type: 'scatter',
+      name: 'Cushion value'
     };
 
-    const data = [trace1];
+    const trace2 = {
+      x: xData,
+      y: yData2,
+      type: 'scatter',
+      name: 'Cycle time'
+    };
 
-    Plotly.newPlot(this.chartId, data);
+    const trace3 = {
+      x: xData,
+      y: yData3,
+      type: 'scatter',
+      name: 'Some value',
+      mode: 'lines+markers',
+            marker: {
+                size: 12,
+                opacity: 0.5
+            }
+    };
+
+    const layout = {
+      title: 'Test data',
+      height: 750,
+      font: {size: 20},
+      xaxis: {
+        title: 'Time',
+      },
+      yaxis: {
+        range: [0, 25],
+        autorange: true
+      },
+      legend: {
+        y: 0.5,
+        font: {size: 16},
+        yref: 'paper'
+      }
+    };
+
+    const data = [trace1, trace2, trace3];
+
+    Plotly.newPlot(this.chartId, data, layout);
   }
 }
